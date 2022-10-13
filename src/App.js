150090fc8417b3cd064602ca17bfa1ea88ea2aa1
GSimic22 <112600678+GSimic22@users.noreply.github.com>
2022-10-13 15:10:24 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import "./App.css";
 import Weather from "./Weather";
-import Footer from "./Footer";
 
 function App() {
   return (
     <div>
       <h1> Weather Search Engine </h1>
       <Weather />
-      <Footer />
+      <footer>
+        <a
+          href="https://github.com/GSimic22/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        , by <a href="https://www.matdelac.io">Gillian Simic</a>
+      </footer>
+      
     </div>
   );
 }
