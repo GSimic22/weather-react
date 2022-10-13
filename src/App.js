7e47d343ae951bc216dfda8403b52dac3ccf9d23
GSimic22 <112600678+GSimic22@users.noreply.github.com>
2022-10-13 15:05:57 -0400

    adding footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Weather from "./Weather";
+import Footer from "./Footer";
+
 function App() {
   return (
     <div>
       <h1> Weather Search Engine </h1>
       <Weather />
+      <Footer />
     </div>
   );
 }
