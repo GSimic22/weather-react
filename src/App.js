fde04cf1aa6fa80a67eda7401e5560efb679630c
GSimic22 <112600678+GSimic22@users.noreply.github.com>
2022-10-18 17:49:06 -0400

    cleaning up app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
 import "./App.css";
-import Weather from "./Weather";
 
 function App() {
-  return (
-    <div>
-      <h1> Weather Search Engine </h1>
-      <Weather />
-      <footer>
-        <a
-          href="https://github.com/GSimic22/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        , by <a href="https://www.matdelac.io">Gillian Simic</a>
-      </footer>
-      
-    </div>
-  );
+  return <div className="App">Weather Search Engine</div>;
 }
 
 export default App;
