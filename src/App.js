97acc36970e4be9ef8c3941974001f1a8e17c8df
GSimic22 <112600678+GSimic22@users.noreply.github.com>
2022-10-19 11:39:59 -0400

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather Search Engine</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        Weather Search Engine
+        <footer>
+          This project was coded by Gillian Simic and is{" "}
+          <a
+            href="https://github.com/GSimic22/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Git-Hub.
+          </a>
+        </footer>
+        ;
+      </div>
+    </div>
+  );
 }
 
 export default App;
