6a7c9d4f7e74ba8537c9b85994e306912749ec3e
GSimic22 <112600678+GSimic22@users.noreply.github.com>
2022-10-19 12:36:41 -0400

    adding css

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import "./App.css";
-
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather Search Engine
+      
+        <Weather />
         <footer>
           This project was coded by Gillian Simic and is{" "}
           <a
             href="https://github.com/GSimic22/weather-react"
             target="_blank"
             rel="noreferrer"
+            className="footer"
           >
             {" "}
             open-sourced on Git-Hub.
           </a>
         </footer>
-        ;
+        
       </div>
     </div>
   );
